caffa8768d7b926aa907779668acd8a94249793e
hellasol <hi@solveig.io>
2020-09-08 16:28:54 +0200

    fix: added image resizer to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const { src, dest, parallel, series} = require('gulp');
 const imagemin = require('gulp-imagemin');
+var responsive = require('gulp-responsive')
+
 
 function buildHtml() {
     return src('src/*.html')
@@ -10,22 +12,59 @@ function buildAssets() {
     return src('src/assets/**')
         .pipe(dest('build/assets/'));
 }
-
-function buildImages() {
+function resizeImages() {
     return src('src/assets/img/**')
-        .pipe(imagemin())
-        .pipe(dest('build/assets/img/'))
-        .pipe(imagemin([
-            imagemin.gifsicle({interlaced: true}),
-            imagemin.mozjpeg({quality: 75, progressive: true}),
-            imagemin.optipng({optimizationLevel: 5}),
-            imagemin.svgo({
-                plugins: [
-                    {removeViewBox: true},
-                    {cleanupIDs: false}
-                ]
-            })
-        ]))
+      .pipe(
+        responsive({
+              '**.jpg': {
+                quality: 50,
+              },
+              '**.png': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+              'chryso_tsokkou_avatar.jpeg': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+              'avatar/**': {
+                quality: 50,
+                compressionLevel: 7,
+                width: 100
+              },
+              'bg/**': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+              'discover/**': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+              'icon/**': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+              'logo/**': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+              'screenshots/**': {
+                quality: 50,
+                compressionLevel: 7,
+              },
+        })
+      )
+    .pipe(dest('build/assets/img/'))
+}
+function buildImages() {
+    return src('build/assets/img/**')
+   
+    .pipe(imagemin([
+        imagemin.mozjpeg({quality: 60}),
+        imagemin.optipng({optimizationLevel: 2}),
+    ]))
+    .pipe(dest('build/assets/img/'))
+        
 }
 
-exports.default = parallel(buildHtml, series(buildAssets, buildImages));
+exports.default = parallel(buildHtml, series(buildAssets, resizeImages, buildImages));
